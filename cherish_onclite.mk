b367f56875620df277d2fcc0a10e84a9968b9c8d
jack2github <jackgithub@yahoo.com>
2021-09-19 17:26:31 +0600

    CherishOS Bringup

--- cherish_onclite.mk
+++ cherish_onclite.mk
@@ -12,21 +12,26 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from onclite device
 $(call inherit-product, device/xiaomi/onclite/device.mk)
 
-# Inherit some common Arrow stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common Cherish stuff.
+$(call inherit-product, vendor/cherish/config/ommon_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 720
-DEVICE_MAINTAINER := bibarub
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_onclite
+PRODUCT_NAME := cherish_onclite
 PRODUCT_DEVICE := onclite
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 7
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "xiaomi/onc_eea/onc:10/QKQ1.191008.001/V11.0.3.0.QFLEUXM:user/release-keys"
+BUILD_FINGERPRINT := "google/barbet/barbet:11/RD2A.210905.002/7513089:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="onc_eea-user 10 QKQ1.191008.001 V11.0.3.0.QFLEUXM release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_GENERIC_PROPERTIES += \
+    ro.cherish.maintainer=Jack
+
+# CherishOS Official
+CHERISH_BUILD_TYPE := OFFICIAL
