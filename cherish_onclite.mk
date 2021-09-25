988757950ac706dafd5a83c582346e4bd02ff209
Jack <89839628+jack2github@users.noreply.github.com>
2021-09-25 14:02:03 +0600

    Fix typo

--- cherish_onclite.mk
+++ cherish_onclite.mk
@@ -13,7 +13,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 $(call inherit-product, device/xiaomi/onclite/device.mk)
 
 # Inherit some common Cherish stuff.
-$(call inherit-product, vendor/cherish/config/ommon_full_phone.mk)
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 720
 
 # Device identifier. This must come after all inclusions.
